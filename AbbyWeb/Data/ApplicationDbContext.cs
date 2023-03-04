09f0a4627172c7b586a5b2040af6b763e83e4e2c
Praik <pratikkumar791@gmail.com>
2023-03-04 15:03:19 +0530

    Add project files.

---
+++ AbbyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AbbyWeb.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace AbbyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)        {
+                
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
